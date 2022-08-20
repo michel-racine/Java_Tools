b11f19fa7c188df6e46276990aecb68a0cf80742
root <root@LAPTOP-5QD6EPGN.localdomain>
2022-08-20 19:50:47 -0400

    Experimental changes made for testing purposes

---
+++ Server.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+public class Test {
+
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/topsecret", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "<body style='background-color:#666666; color:red;'><h1 style='font-size:80px;'>Yes!<br>Mike is the man!</h1><script>alert('[+] Before proceeding, please confirm that Mike is indeed the fucking man with the boss ass master plan.');</script></body>";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
+}
