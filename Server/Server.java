20e5e8f3c0a9f9ccf9f9d67b04a840790c630e1d
root <root@DESKTOP-CE92VQ1.localdomain>
2022-08-20 20:49:34 -0400

    Server mainenance

--- Server/Server.java
+++ Server/Server.java
@@ -10,7 +10,8 @@ public class Test {
 
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/topsecret", new MyHandler());
+	server.createContext("/", new MyHandler());
+        server.createContext("/topsecret", new MyHandler2());
         server.setExecutor(null); // creates a default executor
         server.start();
     }
@@ -24,5 +25,17 @@ public class Test {
             os.write(response.getBytes());
             os.close();
         }
+    }
+
+    static class MyHandler2 implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "<body style='background-color:#0A0A0A; color:blue;'><h1 style='font-size:80px;'>Yes!<br>the secret phrase is ABRACADABRA!</h1><script>alert('[+] Before proceeding, please confirm that you can keep a secret.');</script></body>";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+
     }
 }
